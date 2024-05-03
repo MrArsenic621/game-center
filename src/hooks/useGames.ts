b74f9dc623afe8967c9009d84c868a2447438fa9
MrArsenic621 <asedighi81@gmail.com>
2024-05-03 21:31:08 +0330

    remove unused imports

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,4 +1,4 @@
-import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery } from "@tanstack/react-query";
 import { GameQuery } from "../App";
 import { Platform } from "./usePlatform";
 import APIClient, { FetchResponse } from "../services/api-client";
